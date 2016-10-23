3a49a156f53fdaf11b734ad55b069ac428dcf6c9
liawesomesaucer <vincent.oddrepublic@gmail.com>
2016-10-22 20:11:37 -0700

    Fix flask login issue

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sys
 
 from flask import Flask
 from flask import render_template, flash, redirect, request
-from flask.ext.login import (LoginManager, login_required, 
+from flask_login import (LoginManager, login_required, 
 					login_user, logout_user, current_user)
 
 app = Flask(__name__)
